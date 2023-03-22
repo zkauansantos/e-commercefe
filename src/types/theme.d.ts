5dccdb97b08d87d853d820835fecdb37c3ab3da8
Kauan Santos <kauansantosdepontes@gmail.com>
2023-03-22 20:14:17 -0300

    added more secondaries colors

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -5,7 +5,11 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
       bgDark: string
+      bgSemiDark: string
       bgLight: string
+      bgSemiLight: string
+      dark: string
+      separator: string
       details: string
       text: string
     }
