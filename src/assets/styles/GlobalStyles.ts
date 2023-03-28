0ba76709d644ac889438263043e76c66aa3bc58a
Kauan Santos <kauansantosdepontes@gmail.com>
2023-03-28 15:00:10 -0300

    componentized hero

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -7,4 +7,21 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Rubik', 'Roboto', sans-serif;
   }
+
+  body {
+    overflow-x: hidden;
+  }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.bgSemiDark};
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: ${({ theme }) => theme.colors.separator};
+  }
 `;
