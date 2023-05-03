a7e7b30b59e9d689e608c22add387179db0d3879
Kauan Santos <kauansantosdepontes@gmail.com>
2023-05-03 11:59:44 -0300

    removed content of the tab content

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -46,48 +46,9 @@ export default function Product() {
                 Password
               </Tabs.Trigger>
             </Tabs.List>
-            <Tabs.Content className="TabsContent" value="tab1">
-              <p className="Text">Make changes to your account here. Click save when youre done.</p>
-              <fieldset className="Fieldset">
-                <label className="Label" htmlFor="name">
-                  Name
-                </label>
-                <input className="Input" id="name" defaultValue="Pedro Duarte" />
-              </fieldset>
-              <fieldset className="Fieldset">
-                <label className="Label" htmlFor="username">
-                  Username
-                </label>
-                <input className="Input" id="username" defaultValue="@peduarte" />
-              </fieldset>
-              <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-                <button type="button" className="Button green">Save changes</button>
-              </div>
-            </Tabs.Content>
-            <Tabs.Content className="TabsContent" value="tab2">
-              <p className="Text">Change your password here. After saving, youll be logged out.</p>
-              <fieldset className="Fieldset">
-                <label className="Label" htmlFor="currentPassword">
-                  Current password
-                </label>
-                <input className="Input" id="currentPassword" type="password" />
-              </fieldset>
-              <fieldset className="Fieldset">
-                <label className="Label" htmlFor="newPassword">
-                  New password
-                </label>
-                <input className="Input" id="newPassword" type="password" />
-              </fieldset>
-              <fieldset className="Fieldset">
-                <label className="Label" htmlFor="confirmPassword">
-                  Confirm password
-                </label>
-                <input className="Input" id="confirmPassword" type="password" />
-              </fieldset>
-              <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-                <button type="button" className="Button green">Change password</button>
-              </div>
-            </Tabs.Content>
+
+            <Tabs.Content className="TabsContent" value="tab1" />
+            <Tabs.Content className="TabsContent" value="tab2" />
           </Tabs.Root>
         </ContentDescription>
         <ShareContainer />
